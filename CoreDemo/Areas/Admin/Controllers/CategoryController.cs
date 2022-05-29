0e2f574df97913892c757398fdf4a74b5d95abc2
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-05-29 20:58:51 +0300

    29.05.22

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,14 +11,49 @@ using System.Threading.Tasks;
 using X.PagedList;
 namespace CoreDemo.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        [Area("Admin")]
         public IActionResult Index(int page=1)
         {
             var values = cm.GetList().ToPagedList(page,3);
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(p);
+            if (results.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+
+        public IActionResult CategoryDelete(int id)
+        {
+            var value = cm.GetById(id);
+            cm.TDelete(value);
+            return RedirectToAction("Index");
         }
     }
 }
